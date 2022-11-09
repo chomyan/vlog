ca905735ddd4fe70cae5cdfdfb67def7ba0ba7d9
chomyan <tsoi.rv@gmail.com>
2022-11-09 11:28:44 +0600

    yeah buddy

--- vlog/vlog/settings.py
+++ vlog/vlog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts', #приложение posts**
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
